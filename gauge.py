7a00a5052f67c58853a784bbb8057f3a3a980a3e
Edward Cree <ec429@cantab.net>
2016-02-24 01:55:40 +0000

    gauge: added a bunch more gauges and refactored stuff for them

--- gauge.py
+++ gauge.py
@@ -25,43 +25,94 @@ class Gauge(object):
 		if key not in self.props:
 			return default
 		return self.dl.get(self.props[key], default)
-	def centext(self, txt):
-		return txt.center(self.width - 2)[:self.width - 2]
 	def draw(self):
 		self.cw.clear()
 		self.cw.border()
 	def post_draw(self):
 		self.cw.refresh()
 
-class PercentageGauge(Gauge):
+class OneLineGauge(Gauge):
+	def __init__(self, dl, cw):
+		super(OneLineGauge, self).__init__(dl, cw)
+		self.bordered = self.height > 2
+		if self.bordered:
+			self.height -= 2
+			self.width -= 2
+		self.olg_width = self.width
+	def centext(self, txt):
+		w = self.olg_width
+		return txt.center(w)[:w]
+	def draw(self):
+		self.cw.clear()
+		if self.bordered:
+			self.cw.border()
+	def addstr(self, txt):
+		off = int(self.bordered)
+		self.cw.addstr(off, off, txt[:self.olg_width])
+	def chgat(self, xoff, num, attr):
+		off = int(self.bordered)
+		num = min(num, self.olg_width - xoff)
+		if num > 0:
+			self.cw.chgat(off, off + xoff, num, attr)
+
+class StatusReadout(OneLineGauge):
+	def __init__(self, dl, cw, label):
+		super(StatusReadout, self).__init__(dl, cw)
+		self.label = label
+		self.width -= len(label)
+		self.text = ''.ljust(self.width)
+	def draw(self):
+		super(StatusReadout, self).draw()
+		self.addstr(self.label + self.text)
+	def push(self, txt):
+		self.text = ("%s < %s"%(self.text, txt))[-self.width:]
+
+class TimeGauge(OneLineGauge):
+	def __init__(self, dl, cw):
+		super(TimeGauge, self).__init__(dl, cw)
+		self.add_prop('T', 'v.missionTime')
+	def draw(self):
+		super(TimeGauge, self).draw()
+		t = self.get('T')
+		s = t % 60
+		m = (t / 60) % 60
+		h = (t / 3600)
+		if h < 24:
+			self.addstr('T+%02d:%02d:%02d'%(h, m, s))
+		else:
+			d = h / 24
+			h %= 24
+			self.addstr('T+%02dd%02d:%02d'%(d, h, m))
+
+class PercentageGauge(OneLineGauge):
 	def draw(self, n, d, s):
 		super(PercentageGauge, self).draw()
 		if d is None or d < 0:
-			if self.width < 9:
+			if self.width < 7:
 				text = "N/A"
 			else:
 				text = "N/A: " + s
-			self.cw.addstr(1, 1, self.centext(text))
-			self.cw.chgat(1, 1, self.width - 2, curses.color_pair(0)|curses.A_BOLD)
+			self.addstr(self.centext(text))
+			self.chgat(0, self.width, curses.color_pair(0)|curses.A_BOLD)
 			return
 		percent = n * 100.0 / d if d > 0 else 0
-		if self.width < 6:
+		if self.width < 4:
 			text = " " * (self.width - 2)
-		elif self.width < 8:
+		elif self.width < 6:
 			text = "%03d%%"%(percent,)
 		else:
-			prec = min(3, self.width - 7)
+			prec = min(3, self.width - 5)
 			num = "%0*.*f%%"%(prec + 4, prec, percent)
 			text = "%s %s"%(num, s)
-		self.cw.addstr(1, 1, text[:self.width - 2])
-		blocks = (self.width - 2) * 3
+		self.addstr(text[:self.width])
+		blocks = self.width * 3
 		filled = int(blocks * percent / 100.0 + 0.5)
 		green = int(filled / 3)
-		self.cw.chgat(1, 1, green, curses.color_pair(3))
-		if green < self.width - 2:
-			self.cw.chgat(1, green + 1, 1, curses.color_pair(filled % 3))
-		if green < self.width - 3:
-			self.cw.chgat(1, green + 2, self.width - green - 3, curses.color_pair(0))
+		self.chgat(0, green, curses.color_pair(3))
+		if green < self.width:
+			self.chgat(green, 1, curses.color_pair(filled % 3))
+		if green < self.width - 1:
+			self.chgat(green + 1, self.width - green - 1, curses.color_pair(0))
 
 class FuelGauge(PercentageGauge):
 	def __init__(self, dl, cw, resource):
@@ -69,32 +120,67 @@ class FuelGauge(PercentageGauge):
 		self.resource = resource
 		self.add_prop('current', "r.resourceCurrent[%s]"%(self.resource,))
 		self.add_prop('max', "r.resourceCurrentMax[%s]"%(self.resource,))
+		self.zero = False
 	def draw(self):
 		current = self.get('current')
 		full = self.get('max')
 		super(FuelGauge, self).draw(current, full, self.resource)
+		if current < 0.01 and full > 0:
+			if not self.zero:
+				self.zero = True
+				return 'Stage %s exhausted'%(self.resource,)
+		else:
+			self.zero = False
+
+class GaugeGroup(object):
+	def __init__(self, cw, gl, title):
+		self.cw = cw
+		self.gl = gl
+		self.title = title
+	def draw(self):
+		self.cw.clear()
+		self.cw.border()
+		if self.title:
+			_, width = self.cw.getmaxyx()
+			title = self.title[:width]
+			mid = (width - len(title)) / 2
+			self.cw.addstr(0, mid, title)
+		messages = []
+		for g in self.gl:
+			m = g.draw()
+			if m is not None:
+				messages.append(m)
+	def post_draw(self):
+		for g in self.gl:
+			g.post_draw()
+		self.cw.refresh()
 
 if __name__ == '__main__':
 	import downlink
 	scr = curses.initscr()
-	register_colours()
-	dl = downlink.connect_default()
-	gauges = [
-		FuelGauge(dl, scr.derwin(3, 27, 1, 1), 'LiquidFuel'),
-		FuelGauge(dl, scr.derwin(3, 27, 4, 1), 'Oxidizer'),
-		]
-	status = "Nominal"
 	try:
+		register_colours()
+		dl = downlink.connect_default()
+		scr.addstr(0, 31, "KONRAD: Gauges demo")
+		fuel = scr.derwin(4, 27, 10, 53)
+		fuelgroup = GaugeGroup(fuel, [
+			FuelGauge(dl, fuel.derwin(1, 25, 1, 1), 'LiquidFuel'),
+			FuelGauge(dl, fuel.derwin(1, 25, 2, 1), 'Oxidizer'),
+			], 'Stage Propellant')
+		status = StatusReadout(dl, scr.derwin(1, 78, 23, 1), 'status:')
+		status.push("Nominal")
+		time = TimeGauge(dl, scr.derwin(3, 12, 0, 68))
+		groups = [fuelgroup, status, time]
 		while True:
 			dl.update()
-			for g in gauges:
+			for g in groups:
 				try:
-					g.draw()
+					m = g.draw()
 					g.post_draw()
+					if m is not None:
+						status.push(m)
 				except curses.error as e:
-					status = "ERR: " + repr(e)
-			scr.addstr(23, 1, "status: " + status.ljust(68)[:68])
+					status.push("dpyerr " + repr(e))
 			scr.refresh()
-	except:
+	finally:
 		curses.endwin()
-		raise
